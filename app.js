6e706f995f6af698bbc9c5e04c8ae2162ad3905a
Simon Cheam <simoncheam@Simons-MacBook-Pro.local>
2021-06-30 20:47:59 -0400

    Final Commit

--- app.js
+++ app.js
@@ -1,99 +1,125 @@
 
 // use ID to get value
 const btn = $('#btnSubmit');
-const $h2ID = $('#h2');
+const $body = $(document.body);
 const $textInput = $('#textInput');
-const $textHeader2=$('<h2 id="h2"></h2>');
+
 const $textDiv = $('<div></div>');
+$($body).append($textDiv); //append text div from the start
+const $textUl = $('<ul></ul>');
 
-//const body
-const $body = $('#bodyID')
-
-
-$(document).ready(function() {
-
-
-btn.click(function(){  //fire button click event function
+//fire button click event function 
+btn.click(function(event){   
+    
+    // must create new H2, then append to $textDiv
+   // const $textHeader2=$('<h2></h2>');   ///commented out for #13
+    const $textHeader2=$('<ul></ul>');   ///commented out for #13
+    
+    // comment out h2 above and create a list instead
+    const $li = $('<li></li>');
+    
     event.preventDefault();
-    const textValueFromJQuery = $(textInput).val(); // calls jquery value
+    console.log(event.target);  // log click target
+    
+    const textValueFromJQuery = $($textInput).val(); // calls jquery value
     
     if ( textValueFromJQuery !== ""){  //check if NOT blank
         
-        console.log(textValueFromJQuery); //console logs value
+        $($textHeader2).text(textValueFromJQuery);
+        //$($textDiv).append($textHeader2); 
+
+        $($li).text(textValueFromJQuery);
+        $($textUl).append($li);
+        $($textDiv).append($textUl); 
+
+        //Add hover rules /// H2
+        $($textHeader2).hover( 
+            function() {
+                $($textHeader2).removeClass('bg-light');
+                $($textHeader2).addClass('bg-danger');
+                $($textHeader2).addClass('rounded-pill');
         
-       // alert(textValueFromJQuery);  //disabled alert to eval h2 append
+            },
+            function() {
+                $($textHeader2).removeClass('bg-light');
+                $($textHeader2).removeClass('rounded-pill');
+            })
+
+
+        // click on li elem >>> assign random color
+        const r = Math.floor(Math.random()*256);
+        const g = Math.floor(Math.random()*256);
+        const b = Math.floor(Math.random()*256);
+
+        // single click assigns color
+        $li.click(function(event){
+            $($li).css('color','rgb(' + r + ', ' + g + ', ' + b + ')');
+        });
         
-        
-        //append div to body
-        $($textHeader2).append($textInput);
-        $($textDiv).append($textHeader2); /// this is correct?
-        $($body).append($textDiv);
-        
-        /// Q: why does the h2 not stay appended to the div? (page reloads after hitting okay)
-        
-        console.log("appended text div to body");
-        
+
+        // dblclick on li elem >>> remove item
+        $li.dblclick(function(event){
+            $($li).remove();
+        });
+
         
     } else {
 
         //disabled test 0 // this one works
                 $('#btnSubmit').prop( "disabled", true );
-                console.log("disabled: reload page and enter some text this time!!");
-                
 
-                // disabled test 1 //this one doesn't work...why?
-                $('#btnSubmit').on('click',function() {
-                    $('#btnSubmit').prop("disabled",true);
+                const form = $('.form-group')[0];
+                const reloadBtn = $('<button class="btn btn-outline-danger"> Reload? </button>');
+                $(form).append(reloadBtn);
 
-                    console.log("disabled: enter some text!");
-                
+                alert("Yo asshole you need to enter a value. Gotta hit reload and use your brain though:)");
+
+                //reload button function
+                $(reloadBtn).click(function(event) {
+                    event.preventDefault()
+
+                    //nested confirm statements
+                    const userWantsToReload = confirm('Are you sure you want to reload?');
+                    if(userWantsToReload) {
+
+                        const userReallyWantsToReload = confirm('Are you REALLY sure you want to reload?');
+                        if(userReallyWantsToReload) {
+                        
+
+                        window.location.reload();
+                        }
+                    }
+
+
                 });
-                
-                
+
+
+
+
+
+                // console.log("disabled: reload page and enter some text this time!!");
+                // alert("disabled: reload page and enter some text this time!!");
+            
                 
             }
             
             
-            //test from stack overflow
-        (function myLoop(i) {
-            setTimeout(function() {
-              console.log('hello'); //  your code here                
-              if (--i) myLoop(i);   //  decrement i and call myLoop again if i > 0
-            }, 3000)
-          })(100);  //  pass the number of iterations as an argument
-
+    
           
           //append div to body ///REDUNDANT CODE
-          $($textHeader2).append($textInput);
-          $($textDiv).append($textHeader2); /// this is correct?
-          $($body).append($textDiv);
           
-          /// Q: why does the h2 not stay appended to the div? (page reloads after hitting okay)
+        
           
 });
-
 
 // mouse over h2 elem >>> sign new bg color and border radius
 
 
-$($h2ID).hover( 
-    function() {
-        $($h2ID).addClass('bg-primary');
-        $($h2ID).addClass('rounded-pill');
-
-    },
-    function() {
-        $($h2ID).removeClass('bg-light');
-        $($h2ID).removeClass('rounded-pill');
-    
-    
-    })
-
     
 
 
 
-});
+;
 
 
 ///END
